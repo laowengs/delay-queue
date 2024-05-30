607b5f883572a3043dc9ee09f7630b32a2ead649
wengjp <229595412@qq.com>
2024-05-30 15:00:20 +0800

    CuratorCache

--- delay-queue-service/src/main/java/io/github/laowengs/delay/queue/service/listenter/DelayQueueCuratorCacheListener.java
+++ delay-queue-service/src/main/java/io/github/laowengs/delay/queue/service/listenter/DelayQueueCuratorCacheListener.java
@@ -1,13 +1,28 @@
 package io.github.laowengs.delay.queue.service.listenter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class DelayQueueCuratorCacheListener implements CuratorCacheListener {
     @Override
     public void event(Type type, ChildData oldData, ChildData data) {
+        String path = oldData.getPath();
+        log.info("receive watch type {} for path {}",type,path);
+        switch (type){
+            case NODE_CHANGED:
+
+                break;
+            case NODE_CREATED:
+                break;
+            case NODE_DELETED:
+                break;
+            default:
+                break;
+        }
 
     }
 }
